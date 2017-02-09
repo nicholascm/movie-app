1d30f2869325e17cf866008e0eeecfd01e0420c5
nicholascm90 <nicholascm90@gmail.com>
2017-02-08 22:24:16 -0500

    Add project files.

---
+++ src/MovieWebApplication/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieWebApplication.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
