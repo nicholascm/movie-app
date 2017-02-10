6716a3617f9856fc938fa9acf1fe37d912629b00
nicholascm90 <nicholascm90@gmail.com>
2017-02-10 06:57:25 -0500

    search by genre or query string

--- src/MovieWebApplication/Models/Movie.cs
+++ src/MovieWebApplication/Models/Movie.cs
@@ -11,6 +11,7 @@ namespace MovieWebApplication.Models
         public int ID { get; set; }
         public string Title { get; set; }
         [Display(Name="Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
